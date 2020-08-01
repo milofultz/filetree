c96418b73d61a8a8a675ba8fec3386447226b671
Milo Fultz <milopfultz@gmail.com>
2020-08-01 14:51:46 -0700

    Added tree structure
    Tree structure added, but still trailing pipes are making me annoyed.

--- filetree.py
+++ filetree.py
@@ -8,26 +8,46 @@ def make_list(fp: str) -> list:
     return contents
 
 def print_items(fp: str, contents: list, indent: int) -> None:
+    tree_base = ''
+    for i in range(1, indent+1):
+        tree_base += ' '
+        if i%4 == 0 and i != 0:
+            tree_base += '|'
+
     files = []
+    folders = []
 
     for item in contents:
         if os.path.isdir(os.path.join(fp, item)):
-            print(f"{indent*' '}{item}")
-            print_file_tree(
-                os.path.join(fp, item),
-                indent+2
-            )
+            folders.append(item)
         else:
             files.append(item)
 
-    for file in files: print(f"{indent*' '}{file}")
+    for folder in folders:
+        d = f"{tree_base}--- {folder}"
+        if folder == folders[-1] and not files:
+            d = '\\'.join(d.rsplit('|', 1))
+        print(d)
+        print_file_tree(
+                os.path.join(fp, folder),
+                indent+4
+            )
 
-def print_file_tree(fp: str, indent: int = 0) -> None:
+    for file in files:
+        f = f"{tree_base}--- {file}"
+        if file == files[-1]:
+            f = '\\'.join(f.rsplit('|', 1)) + f'\n{tree_base[:-1]}'
+        print(f)
+
+def print_file_tree(fp: str, indent: int = 4) -> None:
     contents = make_list(fp)
     print_items(fp, contents, indent)
+
 
 
 if __name__ == "__main__":
     cwd = os.getcwd()
 
+    last = 0
+    print(f'.\n`-- {cwd}')
     print_file_tree(cwd)
